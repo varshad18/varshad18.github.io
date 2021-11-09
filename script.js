8221b76651d17dc12c38417bd934fd4859ba3ce0
varshad18 <varshashree73@gmail.com>
2021-11-09 17:23:12 +0530

    commit2

--- script.js
+++ script.js
@@ -33,26 +33,13 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    
 
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
-        autoplay: true,
+        autoplay: false,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
